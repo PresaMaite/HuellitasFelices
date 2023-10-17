1736d702940113f7ad4448fcf0dc7c78277bc3d9
PresaMaite <m.presa@hotmail.com>
2023-10-17 19:54:52 +0200

    login input

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import "./Input.css";
+
+
+export const Input = ({value, type, name, placeholder}) => (
+
+    <input className='loginInput'
+        type={type} 
+        value={value} 
+        name={name} 
+        placeholder={placeholder} 
+    />
+
+)
+
+Input.propTypes = {
+    value: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    placeholder: PropTypes.string
+  };
