c561d790bcd369173b17c29dc274980cb016a97f
Marina <marieneflyaway@gmail.com>
2023-10-22 20:03:35 +0200

    avances buttonpetselect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
-import { BtnPetSelect } from './components/BtnPetSelect/BtnPetSelect'
-
-
+import "./App.css";
+import BtnPetSelect from "./components/BtnPetSelect/BtnPetSelect";
+import React, { useState } from "react";
 
 function App() {
 
+   const animals = [
+    { type: "CAT", description: "Cat 1", image: "assets/cats/cat1.jpg" },
+    { type: "DOG", description: "Dog 1", image: "assets/d/dog1.jpg" },
+  
+   ];
   return (
     <>
-      
+      <BtnPetSelect animals={animals}></BtnPetSelect>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/BtnPetSelect/BtnPetSelect.jsx
+++ src/components/BtnPetSelect/BtnPetSelect.jsx
@@ -1,9 +1,64 @@
 import "./BtnPetSelect.css";
+import React, { useState } from "react";
 
-export const BtnPetSelect = ({ text, onClick }) => {
+function BtnPetSelect({animals}) {
+  const [selectedAnimals, setSelectedAnimals] = useState(animals);
+  console.log(selectedAnimals);
+  console.log(animals);
+
+ 
+
+  //  BtnPetSelect = () => {
+  //   setIsActive(!isActive);
+  //   props.onClick(); // Llama a la función `onClick` proporcionada por el componente padre
+  // };
+
+  const selectAll = () => {
+    console.log("selecting all");
+  };
+
+  const selectCats = () => {
+    console.log("selecting cats");
+    setSelectedAnimals (animals.filter((animal) => animal.type === "CAT"));
+    console.log("Selected cats: ", selectedAnimals);
+  };
+
+  const selectDogs = () => {
+    console.log("selecting dogs");
+    setSelectedAnimals (animals.filter((animal) => animal.type === "DOG"));
+    console.log("Selected cats: ", selectedAnimals);
+  };
+
   return (
-    <button className="petselect" onClick={onClick}>
-      {text}
-    </button>
-  )
+    <div>
+      <div id="buttons">
+        <button
+          onClick={selectAll}
+          className="active"
+         
+        >
+          Todos
+        </button>
+        <button
+          onClick={selectCats}
+         
+        >
+          Gatos
+        </button>
+        <button
+          onClick={selectDogs}
+         
+        >
+          Perros
+        </button>
+      </div>
+      <div id="animals-list">
+        {selectedAnimals.map((animal, index) => (
+          <div key={index}>{animal.description}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default BtnPetSelect;
